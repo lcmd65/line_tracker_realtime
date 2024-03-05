4364c112db9f43818517bafe990ffad0b82721bc
Dat <dat.lemindast@gmail.com>
2024-03-05 13:52:18 +0700

    change

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
-import PyQt6
 import sys
 from PyQt6.QtWidgets import (
     QApplication
 )
 import app
 
-
 if __name__ == "__main__":
     screen = app.templates.home.HomePage()
     application = QApplication(sys.argv)
     screen.show()
     application.exec()
+    
