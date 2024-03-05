25265221b81dbda30694c718e2f86fc2f69fd14e
Dat <dat.lemindast@gmail.com>
2024-03-05 16:53:16 +0700

    update component

--- app/blogging/views/home.py
+++ app/blogging/views/home.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5 import QtWebEngineWidgets
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
 from pathlib import Path
 
 
@@ -7,10 +9,14 @@ class HomePage(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Feelings App Prototype")
+        self.setWindowTitle("Home")
+        view = QWebEngineView()
 
-        view = QtWebEngineWidgets.QWebEngineView()
-        html = Path('app/templates/home.html').read_text(encoding="utf8")
-        view.setHtml(html)
+        html_content = Path(
+            'app/templates/home.html').read_text(encoding="utf8")
+
+        html_content = html_content.format(
+            css_path = QUrl.fromLocalFile("app/static/css/home.css").toString())
+
+        view.setHtml(html_content)
         self.setCentralWidget(view)
-
