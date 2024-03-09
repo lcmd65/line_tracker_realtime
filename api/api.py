5ddece6edb841cf6b36d78130e9ec32d40ac8f75
Dat <dat.lemindast@gmail.com>
2024-03-09 13:24:30 +0700

    change

--- api/api.py
+++ api/api.py
@@ -11,10 +11,10 @@ import bson
 
 api_blueprint = Blueprint("api_blueprint", __name__)
 
-@api_blueprint.route('\home_click', method = ["POST", "GET"])
+@api_blueprint.route('\processing', method = ["POST", "GET"])
 def home_click():
     if request.method == "POST":
-        return render_template("app/templates/home.html")
+        
         
         
         
--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,33 @@
+
 import sys
-from PyQt6.QtWidgets import (
-    QApplication
-)
-from PyQt6.QtGui import (
-    QIcon
-)
+import os
+from flask import (Flask,
+                   Blueprint,
+                   render_template,
+                   redirect, request,
+                   url_for)
+from flask_sqlalchemy import SQLAlchemy
+from flask_caching import Cache
+from openai import OpenAI
+import warnings
+warnings.filterwarnings("ignore")
 import app.blogging.views.home
-import flask
-from flask import Flask, session
-from api.api import api_blueprint
+from PyQt6.QtWidgets import QApplication, QIcon
 
 def main():
-    app = Flask(__name__)
-    app.config(
-        {
-            "PORT": "8080",
-            "user": "dat",
-            "password": "123456",
-        }
+    app = Flask(__name__, instance_relative_config= True)
+    app.config.from_mapping(
+        SECRET_KEY='unilever',
+        DATABASE= os.path.join(app.instance_path, 'flaskr.sqlite'),
+        CACHE_TYPE='FileSystemCache',
+        CACHE_DIR='cache',
+        CACHE_THRESHOLD=100000,
     )
-    app.blueprints()
-    
+    from app.blogging.views.home import home
+    from app.authentication.views.authen import authen
+    app.register_blueprint(home)
+    app.register_blueprint(authen)
+
     application = QApplication(sys.argv)
     app_icon = QIcon("app/static/images/app-icon.png")
     application.setWindowIcon(app_icon)
